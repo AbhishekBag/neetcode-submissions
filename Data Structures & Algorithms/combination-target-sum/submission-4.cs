14326f2b83d049d6b6a94303b1ce6fb202ce5f69
Abhishek <48788855+AbhishekBag@users.noreply.github.com>
2026-09-25 23:44:51 +0530

    Add: combination-target-sum - submission-4

---
+++ Data Structures & Algorithms/combination-target-sum/submission-4.cs
@@ -0,0 +1,30 @@
+public class Solution {
+    private List<List<int>> res;
+    public List<List<int>> CombinationSum(int[] nums, int target) {
+        res = new List<List<int>>();
+        // for(int i = 0; i < nums.Length; i++) {
+        //     GetCombination(nums, i, target - nums[i], new List<int> { nums[i] });
+        // }
+
+        GetCombination(nums, 0, target, new List<int>());
+
+        return res;
+    }
+
+    private void GetCombination(int[] nums, int start, int target, List<int> cur) {
+        if(target < 0) {
+            return;
+        }
+
+        if(target == 0) {
+            res.Add(cur.ToList());
+            return;
+        }
+
+        for(int i = start; i < nums.Length; i++) {
+            cur.Add(nums[i]);
+            GetCombination(nums, i, target - nums[i], cur);
+            cur.RemoveAt(cur.Count - 1);
+        }
+    }
+}
